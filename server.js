1501ffe27a0d513d3a27885d61c50705b552ad41
Cole Hart <cole@colehart.com>
2018-11-25 18:28:18 -0700

    Attempt to create sunsets route and complete 404 error

--- server.js
+++ server.js
@@ -11,15 +11,21 @@ const timeLogger = (request, response, next) => {
 };
 
 app.use(urlLogger, timeLogger);
-app.use(express.static('public'));
-
-app.get('/', (request, response) => {
-
-});
+app.use(express.static(__dirname + '/public'));
+// app.get('/', (request, response) => {
+// });
 
 app.get('/json', (request, response) => {
   response.status(200).json({'name': 'Cole'});
 });
+
+app.get('/sunsets', (request, response) => {
+  response.sendFile('sunsets.html');
+});
+
+app.use((request, response, next) => {
+  response.status(404).send('Sorry, the path you entered does not exist.')
+})
 
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
